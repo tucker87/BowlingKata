4345af21437216ae86b9b7fd7a843016be246094
Jason Tucker <jtgeek@gmail.com>
2015-11-15 05:09:24 -0500

    Version 2. Much cleaner. Want more OO solution.

---
+++ BowlingKata2/Game.cs
@@ -0,0 +1,58 @@
+﻿namespace BowlingKata2
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _rollIndex;
+
+        public void Roll(int pins)
+        {
+            _rolls[_rollIndex] = pins;
+            _rollIndex += IsStrike(_rollIndex) ? 2 : 1;
+        }
+
+        public int Score
+        {
+            get
+            {
+                var score = 0;
+                for (var currentFrame = 0; currentFrame < 20; currentFrame += 2)
+                {
+                    if (IsStrike(currentFrame))
+                        score += GetNextTwoRolls(currentFrame);
+
+                    if (IsSpare(currentFrame))
+                        score += GetNextRoll(currentFrame);
+
+                    score += GetCurrentFrameRolls(currentFrame);
+                }
+                return score;
+            }
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return _rolls[roll] == 10;
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return GetCurrentFrameRolls(roll) == 10;
+        }
+
+        private int GetNextTwoRolls(int roll)
+        {
+            return _rolls[roll + 1] + _rolls[roll + 2];
+        }
+
+        private int GetCurrentFrameRolls(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1];
+        }
+
+        private int GetNextRoll(int roll)
+        {
+            return _rolls[roll + 2];
+        }
+    }
+}
---
+++ BowlingTest2/BowlingKataTest.cs
@@ -0,0 +1,85 @@
+﻿using BowlingKata2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingTest2
+{
+    [TestClass]
+    public class BowlingKataTest
+    {
+        private Game _game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void RollOneScoreOne()
+        {
+            _game.Roll(1);
+            Assert.AreEqual(1, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollTwoScoreTwo()
+        {
+            _game.Roll(2);
+            Assert.AreEqual(2, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollOneRollOneScoreTwo()
+        {
+            RollMany(2, 1);
+            Assert.AreEqual(2, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollSpareRoll1ScoreTwelve()
+        {
+            RollMany(2, 5);
+            _game.Roll(1);
+            Assert.AreEqual(12, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollOneRollOneRollSpareRollOneRollOneScoreFifteen()
+        {
+            RollMany(2, 1);
+            _game.Roll(9);
+            _game.Roll(1);
+            RollMany(2, 1);
+            Assert.AreEqual(15, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollStrikeRollOneRollOneScoreFourteen()
+        {
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(1);
+            Assert.AreEqual(14, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollAllOnesScoreTwenty()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollAllStrikesScoreThreeHundred()
+        {
+            RollMany(11, 10);
+            Assert.AreEqual(300, _game.Score);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+                _game.Roll(pins);
+        }
+    }
+}
