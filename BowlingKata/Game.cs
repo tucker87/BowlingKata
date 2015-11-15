07890dc07d65971c73b78da40eb1f6de7d15194e
Jason Tucker <jtgeek@gmail.com>
2015-11-15 03:49:12 -0500

    Initial Commit

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        public Game()
+        {
+            _frames.Add(new Frame(0, _frames));
+        }
+
+        public int Score => _frames.Sum(frame => frame.Score);
+        private readonly List<Frame> _frames = new List<Frame> ();
+        private int _frameIndex;
+        private Frame CurrentFrame => _frames[_frameIndex];
+
+
+        public void Roll(int i)
+        {
+            CurrentFrame.Add(i);
+
+            if (CurrentFrame.Rolls[1] != null || i == 10)
+            {
+                _frameIndex++;
+                _frames.Add(new Frame(_frameIndex, _frames));
+            }
+        }
+    }
+
+    public class Frame
+    {
+        public Frame(int frameIndex, List<Frame> frames)
+        {
+            Index = frameIndex;
+            Frames = frames;
+        }
+
+        internal readonly List<Frame> Frames;
+        public int Index { get; set; }
+        public int?[] Rolls = {null, null};
+
+        public int RollTotal => (Rolls[0] ?? 0) + (Rolls[1] ?? 0);
+
+        public int Bonus
+        {
+            get
+            {
+                var score = 0;
+                var nextFrame = Index + 1;
+                if (Rolls[0] == 10)
+                {
+                    var rollCount = 0;
+                    while (rollCount < 2 && nextFrame < Frames.Count - 1)
+                    {
+                        foreach (var roll in Frames[nextFrame].Rolls)
+                        {
+                            if (roll > 0)
+                            {
+                                score += (int) roll;
+                                rollCount++;
+                            }
+                        }
+                        nextFrame++;
+                    }
+                }
+
+                if (Rolls[0] + Rolls[1] == 10)
+                    if (Frames.Count > Index + 1)
+                        score = Frames[Index + 1].Rolls[0] ?? 0;
+
+                return score;
+            }
+        }
+
+        public int Score => RollTotal + Bonus;
+
+        private int _rollIndex;
+        public void Add(int i)
+        {
+            Rolls[_rollIndex] = i;
+            _rollIndex++;
+        }
+    }
+}
---
+++ BowlingTest/BowlingKataTest.cs
@@ -0,0 +1,115 @@
+﻿using BowlingKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class BowlingKataTest
+    {
+        private Game _game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void RollOneScoreOne()
+        {
+            _game.Roll(1);
+            Assert.AreEqual(1, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollTwoScoreTwo()
+        {
+            _game.Roll(2);
+            Assert.AreEqual(2, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollOneThenOneScoreTwo()
+        {
+            RollMany(2, 1);
+            Assert.AreEqual(2, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollTwentyOnesScoreTwenty()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollSpareThenOneScoreTwelve()
+        {
+            RollSpare();
+            _game.Roll(1);
+            Assert.AreEqual(12, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollSpareThenTwoScoreFourteen()
+        {
+            RollSpare();
+            _game.Roll(2);
+            Assert.AreEqual(14, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollSpareRollTwoRollOneScoreFifteen()
+        {
+            RollSpare();
+            _game.Roll(2);
+            _game.Roll(1);
+            Assert.AreEqual(15, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollOneRollOneRollSpareRollOneScoreFourteen()
+        {
+            RollMany(2, 1);
+            RollSpare();
+            _game.Roll(1);
+            Assert.AreEqual(14, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollOneRollOneRollSpareRollTwoScoreSixteen()
+        {
+            RollMany(2, 1);
+            RollSpare();
+            _game.Roll(2);
+            Assert.AreEqual(16, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollStrikeRollOneRollTwoScoreSixteen()
+        {
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(2);
+            Assert.AreEqual(16, _game.Score);
+        }
+
+        [TestMethod]
+        public void RollAllStrikes()
+        {
+            RollMany(11, 10);
+            Assert.AreEqual(300, _game.Score);
+        }
+
+        private void RollSpare()
+        {
+            RollMany(2, 5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+                _game.Roll(pins);
+        }
+    }
+}
